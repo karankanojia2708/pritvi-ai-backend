81238361dd84336fddec3fe7eb224f673c3cecd1
karankanojia2708 <karankanojia2708.com>
2020-05-08 00:41:45 +0530

    th comm

--- model/User.js
+++ model/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose'); 
 const Schema = mongoose.Schema; 
 const struct_user = new Schema({
-    eamil : {
-        type : String, require : true, trim : true, minlength : 3 
+    email : {
+        type : String, require : true, trim : true, minlength : 3, unique : true
     }, 
     password : {
         type : String, required : true, trim : true, minlength : 3
